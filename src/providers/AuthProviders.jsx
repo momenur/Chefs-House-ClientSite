62c11b4ed202fa6ac102fcb6c85c13e7bf62915f
momenur <momenurislam6@gmail.com>
2023-05-04 02:07:26 +0600

    added footer

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,11 +1,25 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 
+const Auth = getAuth(app)
+
 const AuthProviders = ({children}) => {
-    const user = {displayName: 'Sagor Nodi'}
+
+    const [user, setUser] = useState(null)
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(Auth, email, password)
+    }
+
+    const AuthInfo = {
+        user,
+        createUser,
+    }
     return (
-        <AuthContext.Provider value={user}>
+        <AuthContext.Provider value={AuthInfo}>
             {children}
         </AuthContext.Provider>
     );
